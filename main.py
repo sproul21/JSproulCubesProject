12b3721a9069acd70f78bed775047f0160064a4d
sproul21 <josh.sproul@gmail.com>
2023-02-02 15:12:51 -0500

    Writes to DB

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#
 # import sys
 # import requests
+# import sqlite3
 # from secrets import wufoo_key
 # from requests.auth import HTTPBasicAuth
 #
@@ -9,32 +9,45 @@
 #     url = "https://jsproul.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
 #     response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
 #
-#     if response.status_code != 200:  # if we don't get an ok response we have trouble
+#     if response.status_code != 200:
 #         print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
 #         sys.exit(-1)
 #     jsonresponse = response.json()
-#     return jsonresponse  # json response will be either a dictionary or a list of dictionaries
+#     return jsonresponse['Entries']
 #
 #
-# # each dictionary represents a json object
 # def write_wufoo_data():
-#     # JSON data
-#     data = get_wufoo_data()['Entries']
+#     # create a database connection
+#     conn = sqlite3.connect('wufoo_data.db')
+#     c = conn.cursor()
 #
-#     # open a file for writing
-#     with open("data.txt", "w") as outfile:
-#         # write the JSON key-value pairs to the file
-#         for item in data:
-#             for key, value in item.items():
-#                 outfile.write(f"{key}: {value}\n")
+#     # create the table if it doesn't already exist
+#     c.execute('''CREATE TABLE IF NOT EXISTS entries
+#                  (Entry_Id text, Title text, First_Name text, Last_Name text,
+#                  Org_Title text, Organization text, Email text, Org_Website text,
+#                  Phone_Number text, Time_Period text, Permission text,
+#                  Opportunities text, Date_Created text, Created_By text,
+#                  Date_Updated text, Updated_By text)''')
 #
-#     # print the file
-#     print_file("data.txt")
+#     # retrieve the data from Wufoo
+#     data = get_wufoo_data()
 #
+#     # insert the data into the table
+#     for item in data:
+#         opportunities = ', '.join([item[f'Field112'], item[f'Field113'], item[f'Field114'],
+#                                   item[f'Field115'], item[f'Field116'], item[f'Field117'],
+#                                   item[f'Field118']])
 #
-# def print_file(file_name):
-#     with open(file_name) as f:
-#         print(f.read())
+#         c.execute("INSERT INTO entries VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+#                   (item['EntryId'], item['Field214'], item['Field1'], item['Field2'], item['Field8'],
+#                    item['Field9'], item['Field5'], item['Field6'], item['Field7'],
+#                    ', '.join([item[f'Field12'], item[f'Field13'], item[f'Field14'],
+#                              item[f'Field15'], item[f'Field16']]), item['Field212'],
+#                    opportunities, item['DateCreated'], item['CreatedBy'], item['DateUpdated'], item['UpdatedBy']))
+#
+#     # commit the changes to the database and close the connection
+#     conn.commit()
+#     conn.close()
 #
 #
 # if __name__ == "__main__":
@@ -46,6 +59,7 @@ import sqlite3
 from secrets import wufoo_key
 from requests.auth import HTTPBasicAuth
 
+
 def get_wufoo_data() -> dict:
     url = "https://jsproul.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
     response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
@@ -55,6 +69,7 @@ def get_wufoo_data() -> dict:
         sys.exit(-1)
     jsonresponse = response.json()
     return jsonresponse['Entries']
+
 
 def write_wufoo_data():
     # create a database connection
@@ -74,22 +89,21 @@ def write_wufoo_data():
 
     # insert the data into the table
     for item in data:
-        opportunities = ', '.join([item[f'Field112'], item[f'Field113'], item[f'Field114'],
-                                  item[f'Field115'], item[f'Field116'], item[f'Field117'],
-                                  item[f'Field118']])
+        opportunities = ', '.join([item.get('Field112', ''), item.get('Field113', ''), item.get('Field114', ''),
+                                  item.get('Field115', ''), item.get('Field116', ''), item.get('Field117', ''),
+                                  item.get('Field118', '')])
 
         c.execute("INSERT INTO entries VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                  (item['EntryId'], item['Field214'], item['Field1'], item['Field2'], item['Field8'],
-                   item['Field9'], item['Field5'], item['Field6'], item['Field7'],
-                   ', '.join([item[f'Field12'], item[f'Field13'], item[f'Field14'],
-                             item[f'Field15'], item[f'Field16']]), item['Field212'],
-                   opportunities, item['DateCreated'], item['CreatedBy'], item['DateUpdated'], item['UpdatedBy']))
+                  (item['EntryId'], item.get('Field214', ''), item.get('Field1', ''), item.get('Field2', ''), item.get('Field8', ''),
+                   item.get('Field9', ''), item.get('Field5', ''), item.get('Field6', ''), item.get('Field7', ''),
+                   ', '.join([item.get(f'Field12', ''), item.get(f'Field13', ''), item.get(f'Field14', ''),
+                             item.get(f'Field15', ''), item.get(f'Field16', '')]), item.get('Field212', ''),
+                   opportunities, item.get('DateCreated', ''), item.get('CreatedBy', ''), item.get('DateUpdated', ''), item.get('UpdatedBy', '')))
 
     # commit the changes to the database and close the connection
     conn.commit()
     conn.close()
 
+
 if __name__ == "__main__":
     write_wufoo_data()
-
-
